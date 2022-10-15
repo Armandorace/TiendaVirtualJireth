90a4ecfa7aee54974566997a902661f651eba045
Armando Racero <arjorasi_03@hotmail.com>
2022-10-15 17:08:44 -0500

    models

--- back/server.js
+++ back/server.js
@@ -1,13 +1,14 @@
 const app=require("./app")
+
 const connectDatabase = require("./config/database");
 
 //Setear el archivo de configuración
 const dotenv=require("dotenv");
-dotenv.config({path: 'back/config/config.env'})
+dotenv.config({path: 'back/config/config.env'})//Existe un archivo de configuracion inicial
 
 //Configurar base de datos
 
-connectDatabase();
+connectDatabase(); 
 
 //llamamos al server
 const server=app.listen(process.env.PORT, () => {
