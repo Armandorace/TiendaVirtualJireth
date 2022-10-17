d15c791bdf12784cab4cfe9f2fb4021bb52207da
Armando Racero <arjorasi_03@hotmail.com>
2022-10-17 16:19:22 -0500

    Agregar Prodeucto

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,3 +1,6 @@
+const producto= require("../models/productos")
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next) => {
     res.status(200).json({
         sucess:true,
@@ -5,3 +8,14 @@ exports.getProducts=(req,res,next) => {
     })
 }
 
+
+//Crear nuevo producto /api/productos
+
+exports.newProduct = async (req,res,next)=> {
+    const product = await producto.create(req.body);
+
+    res.status(201).json({
+       success: true,
+       product 
+    })
+}
