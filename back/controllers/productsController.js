bfe7791b21e79d1c03bd5db001113c6a2e8cf06e
Armando Racero <arjorasi_03@hotmail.com>
2022-10-18 00:16:57 -0500

    funcional con fetch

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,13 +1,70 @@
 const producto= require("../models/productos")
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url));// usurpacion del 
 
 //Ver la lista de productos
-exports.getProducts=(req,res,next) => {
+exports.getProducts= async(req,res,next) => {
+    const productos = await producto.find();    
     res.status(200).json({
         sucess:true,
-        message: "En esta ruta ud va a poder ver todos los productos"
+        count: productos.length,
+        productos
+    })
+}
+//Ver un producto por ID
+
+exports.getProductById = async (req, res, next)=>{
+    const product = await producto.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success:false,
+            message: "No encontramos ese producto"
+        })
+    }
+    res.status(200).json({
+        sucess:true,
+        message: "Aqui debajo encuentras informacion sobre tu producto: ",
+        product
     })
 }
 
+// Update  un producto
+exports.updateProduct = async (req, res, next)=>{
+    let product = await producto.findById(req.params.id)
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: "No encontramos ese producto"
+        })
+    }
+   // Si el objeto si existia, entonces si ejecuta la actualizacion
+    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, // Valido solo los atributos nuevos o actualizado
+        runValidators:true
+    });
+    //Respondo ok si el producto si se actualizo
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
+
+//eliminar producto
+exports.deleteProduct = async (req, res, next)=>{
+    let product = await producto.findById(req.params.id)// Variable de tipo modificable
+    if (!product){ //verifico que el objeto no existe para finalizar el proceso
+            return res.status(404).json({//Si el objeto no existe, return termina el metodo
+            success:false,
+            message: "No encontramos ese producto"
+        })
+    }
+    await product.remove();//Eliminamos el proceso
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente"
+
+    })
+}
 
 //Crear nuevo producto /api/productos
 
@@ -19,3 +76,24 @@ exports.newProduct = async (req,res,next)=> {
        product 
     })
 }
+
+
+
+// Hablemos de FETCH
+//VER TODOS LOS PRODUCTOS
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos();
+//VER POR ID
+function verProductosPorID(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+//verProductosPorID('634dc6528d993e5501dbb6cb'); //probamos ver productos por id
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,13 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct} = require("../controllers/productsController") // traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") // traemos la respuesta json desde el controlador
 
 router.route('/productos').get(getProducts) // Establecemos desde que ruta queeremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);// Establecemos la ruta
+router.route('/producto/:id').get(getProductById);// Ruta para consultar por id
+router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
+router.route('/producto/:id').delete(deleteProduct);//Creacion de ruta para eleiminar por id
+
 
 module.exports=router;
