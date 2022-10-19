8c2b06e313b3c00d665d82e5ce73b30f126268db
Armando Racero <arjorasi_03@hotmail.com>
2022-10-19 00:28:30 -0500

    Front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda Jireth Systar Security</h1>
+      <img src="./images/tiendavirtual.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
