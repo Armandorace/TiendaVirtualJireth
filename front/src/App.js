53ef6ef11b6723b17d70ae75eb51e92b7623cd21
Armando Racero <arjorasi_03@hotmail.com>
2022-10-19 23:22:46 -0500

    Front_Header_Footer
    Pagina

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tienda Jireth Systar Security</h1>
-      <img src="./images/tiendavirtual.png" alt="Aqui va el logo"></img>
+      <Header />
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <Footer />
     </div>
   );
 }
